3198e4b43023ce5e41924410e71b84bf8e636520
Michael Sieler <sielerjm@oregonstate.edu>
2022-07-21 08:16:38 -0700

    update custom.css

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ html_theme_options = {
     "logo_only": True,
     "show_toc_level": 2,
     "announcement": (
-        "🌞 Looking for a summer 2023 internship. <a href = 'https://michaelsieler.com/en/latest/Career/internship.html'>More info here.</a> 🌞"
+        "🌞 Looking for a summer 2023 internship. <a class="reference internal" href = 'https://michaelsieler.com/en/latest/Career/internship.html'>More info here.</a> 🌞"
     ),
 }
 
