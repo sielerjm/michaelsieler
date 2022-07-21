d1e7c5f639da65a40117915c6b2d7d02d26f2228
Michael Sieler <sielerjm@oregonstate.edu>
2022-07-21 05:39:28 -0700

    Installing and testing Book Theme

--- conf.py
+++ conf.py
@@ -54,7 +54,8 @@ html_title = "Michael Sieler"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
 
 
 # Custom styles go here
